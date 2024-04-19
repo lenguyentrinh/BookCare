27eace5fc311a7edc0d55203427561152170639b
lenguyentrinh <lethinguyentrinh.work@gmail.com>
2024-04-19 16:45:30 +0700

    create, display all users

--- backend/src/controllers/homeCotroller.js
+++ backend/src/controllers/homeCotroller.js
@@ -1,21 +1,43 @@
-import db from '../models/index'
+import CRUDService from "../services/CRUDService";
+import db from "../models/index";
+import { json } from "body-parser";
 
-let getHomePage = async(req,res)=>{
-    try {
-        let data = await db.User.findAll();
-        console.log("---------------------------")
-        console.log(data)
-        return res.render('homepage.ejs',{
-            data: JSON.stringify(data)
-        })
-    }catch(e){
-console.log(e)
-    }
-   
-
-}
-
-
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    console.log("---------------------------");
+    console.log(data);
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+let getCRUD = (req, res) => {
+  try {
+    // let data = await db.User.findAll();
+    // console.log("---------------------------");
+    // console.log(data);
+    return res.render("crud.ejs");
+  } catch (e) {
+    console.log(e);
+  }
+};
+let postCRUD = async (req, res) => {
+  let mess = await CRUDService.createNewUser(req.body);
+  console.log(mess);
+  return res.send("post crud");
+};
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  return res.render("displayCRUD.ejs", {
+    dataTable: data,
+  });
+};
 module.exports = {
-    getHomePage : getHomePage,
-}
+  getHomePage: getHomePage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
+};
--- backend/src/route/web.js
+++ backend/src/route/web.js
@@ -1,16 +1,14 @@
 import express from "express";
 import homeCotroller from "../controllers/homeCotroller";
 
-let router  = express.Router();
+let router = express.Router();
 
-let initWebRouters = (app) =>{
-
-    router.get('/', homeCotroller.getHomePage)
-
-
-
-
-    return app.use("/",router)
-}
+let initWebRouters = (app) => {
+  router.get("/", homeCotroller.getHomePage);
+  router.get("/crud", homeCotroller.getCRUD);
+  router.post("/post-crud", homeCotroller.postCRUD);
+  router.get("/get-crud", homeCotroller.displayGetCRUD);
+  return app.use("/", router);
+};
 
 module.exports = initWebRouters;
